0973927eaa77af5287662538c1438ade89a9bca9
babono <babono@hotmail.com>
2024-12-21 13:20:11 +0700

    fix runtime for dynmic pages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 // This is blog list page and should be done by Team Ella
 // Todo: Slicing the design and need to create functionallity to fetch the data from the notion database and render the data on this page. For this one need to read the parameter from the URL and fetch the data based on the parameter. Good Luck!
 import Image from "next/image";
