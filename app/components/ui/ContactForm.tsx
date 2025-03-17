0a01e70272cb64fae123d791d1e1452d52c26e67
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-03-17 15:20:06 +0800

    Add setters to contact form

--- app/components/ui/ContactForm.tsx
+++ app/components/ui/ContactForm.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useState } from "react";
+import Button from "./button";
+
 export default function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <>
       <form
@@ -6,11 +15,23 @@ export default function ContactForm() {
         gap-5"
       >
         <div>
-          <input type="text" id="fullname" placeholder="Name" />
+          <input
+            type="text"
+            id="fullname"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div>
-          <input type="text" id="email" placeholder="Email Address" />
+          <input
+            type="email"
+            id="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div>
@@ -18,15 +39,26 @@ export default function ContactForm() {
             className="h-32"
             id="message"
             placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
         </div>
 
-        <button
+        <Button
+          variant="yellow"
+          rounding="lg"
+          size="md"
+          className="font-semibold !bg-[#EFB61E] shadow-md"
+        >
+          Submit
+        </Button>
+
+        {/* <button
           className="mx-auto w-40 bg-yellow-500 text-white font-bold hover:bg-yellow-600 text-white font-bold py-3 rounded-2xl shadow-md"
           type="submit"
         >
           Submit
-        </button>
+        </button> */}
       </form>
 
       <div className="bg-slate-100 flex flex-col">
