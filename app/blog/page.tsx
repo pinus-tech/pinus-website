3c1a53830abb099ef45481f612379bc7ec544003
Gabriella <gabriella@u.nus.edu>
2025-04-10 22:44:46 +0800

    Add skeleton while loading

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,13 @@
-"use client"
-import { useState, useEffect, Suspense } from "react";
+"use client";
+
+// export const runtime = "edge";
+
+import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { TitleHeader } from "../components/ui/title";
 import Link from "next/link";
+import { TitleHeader } from "../components/ui/title";
+import { BlurFade } from "../components/ui/blur-fade";
+
 import {
   Select,
   SelectContent,
@@ -22,6 +27,7 @@ import {
   CardImage,
   CardTags,
 } from "../components/ui/card";
+import { Skeleton } from "../components/ui/skeleton";
 
 interface Blog {
   id: string;
@@ -36,9 +42,7 @@ interface Blog {
 
 async function getBlogs(): Promise<Blog[]> {
   const res = await fetch("/api/blogs", { cache: "no-store" });
-  if (!res.ok) {
-    throw new Error("Failed to fetch blogs");
-  }
+  if (!res.ok) throw new Error("Failed to fetch blogs");
   return res.json();
 }
 
@@ -48,78 +52,69 @@ const trimNotionURL = (url: string) => {
 };
 
 function BlogListContent() {
+  const BLUR_FADE_DELAY = 0.04;
+
   const [selected, setSelected] = useState<string>("");
   const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const blogsPerPage = 6;  // You can adjust this number to control how many blogs per page
+  const blogsPerPage = 6;
   const router = useRouter();
   const searchParams = useSearchParams();
   const queryType = searchParams.get("type");
 
   useEffect(() => {
-    if (queryType) {
-      setSelected(queryType);
-    }
+    if (queryType) setSelected(queryType);
   }, [queryType]);
 
   useEffect(() => {
     setCurrentPage(1);
   }, [selected]);
-  
+
   useEffect(() => {
     router.push(`?type=${selected}`);
   }, [selected, router]);
 
   useEffect(() => {
+    setLoading(true);
     getBlogs()
       .then(setBlogs)
-      .catch((error) => console.error("Error fetching blogs:", error));
+      .catch((error) => console.error("Error fetching blogs:", error))
+      .finally(() => setLoading(false));
   }, []);
 
-  // Filter blogs based on selected category
-  const filteredBlogs = selected === "all" || !selected
-    ? blogs
-    : blogs.filter((blog) => blog.categories.includes(selected));
+  const filteredBlogs =
+    selected === "all" || !selected
+      ? blogs
+      : blogs.filter((blog) => blog.categories.includes(selected));
 
   const totalPages = Math.ceil(filteredBlogs.length / blogsPerPage);
-  const currentBlogs = filteredBlogs.slice((currentPage - 1) * blogsPerPage, currentPage * blogsPerPage);
+  const currentBlogs = filteredBlogs.slice(
+    (currentPage - 1) * blogsPerPage,
+    currentPage * blogsPerPage
+  );
 
   const handlePageChange = (page: number) => {
-    if (page >= 1 && page <= totalPages) {
-      setCurrentPage(page);
-    }
+    if (page >= 1 && page <= totalPages) setCurrentPage(page);
   };
 
   const generatePagination = () => {
     const pages: (number | string)[] = [];
-    
+
     if (totalPages <= 5) {
-      // If the total pages are 5 or fewer, just show all the pages
-      for (let i = 1; i <= totalPages; i++) {
-        pages.push(i);
-      }
+      for (let i = 1; i <= totalPages; i++) pages.push(i);
     } else {
-      // Always show the first page if it's not the current page
       if (currentPage > 2) pages.push(1);
-  
-      // Show ellipsis if current page is far from the first page
       if (currentPage > 2) pages.push("...");
-  
-      // Show the previous and next page numbers
       if (currentPage > 1) pages.push(currentPage - 1);
       pages.push(currentPage);
       if (currentPage < totalPages) pages.push(currentPage + 1);
-  
-      // Show ellipsis if current page is far from the last page
       if (currentPage < totalPages - 1) pages.push("...");
-  
-      // Always show the last page if it's not the current page
       if (currentPage < totalPages - 1) pages.push(totalPages);
     }
-  
+
     return pages;
   };
-  
 
   return (
     <>
@@ -141,77 +136,109 @@ function BlogListContent() {
         </Select>
       </div>
 
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">      
-        {currentBlogs
-          .filter((blog) => !selected || selected == "all" || blog.categories.includes(selected))
-          .map((blog) => (
-            <Link href={`/blog/${trimNotionURL(blog.url)}`} key={blog.id}>
-              <Card className="overflow-hidden shadow-md flex flex-col h-full">
-                <CardImage
-                  width={400}
-                  height={250}
-                  src={blog.thumbnail || "/blue.png"}
-                  alt={blog.title}
-                  className="w-full object-cover"
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8 px-4 sm:px-0 justify-items-center">
+      {loading
+        ? [...Array(6)].map((_, index) => (
+            <BlurFade
+              delay={BLUR_FADE_DELAY * (index + 1.5)}
+              key={index}
+              className="flex flex-col h-full max-w-xs w-full"
+              inView
+            >
+              <Skeleton className="rounded-xl h-48 w-full" />
+              <Skeleton className="mt-4 h-6 w-3/4" />
+              <Skeleton className="mt-2 h-4 w-full" />
+              <Skeleton className="mt-2 h-4 w-2/3" />
+              <Skeleton className="mt-4 h-6 w-1/4" />
+            </BlurFade>
+          ))
+        : currentBlogs.map((blog, index) => (
+            <BlurFade
+              delay={BLUR_FADE_DELAY * (index + 1.5)}
+              key={blog.id}
+              className="flex flex-col h-full max-w-xs w-full"
+              inView
+            >
+              <Link
+                href={`/blog/${trimNotionURL(blog.url)}`}
+                className="max-w-xs w-full mx-auto"
+              >
+                <Card className="overflow-hidden shadow-md flex flex-col h-full min-h-[420px]">
+                  <CardImage
+                    width={400}
+                    height={250}
+                    src={blog.thumbnail || "/blue.png"}
+                    alt={blog.title}
+                    className="w-full h-48 object-cover"
                   />
-                <CardHeader>
-                  <CardTitle>{blog.title}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <CardDescription>By {blog.author} on {blog.date}</CardDescription>
-                </CardContent>
-                <CardContent>
-                  <CardDescription>{blog.description}</CardDescription>
-                </CardContent>
-                <CardFooter>
-                  <CardTags teamName="" eventType={blog.categories.join(', ')} />
-                </CardFooter>
-              </Card>
-            </Link>
+                  <CardHeader>
+                    <CardTitle className="text-lg line-clamp-2">
+                      {blog.title}
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex-grow">
+                    <CardDescription className="text-sm text-gray-600">
+                      By {blog.author} on {blog.date}
+                    </CardDescription>
+                    <p className="mt-2 text-sm line-clamp-3 text-gray-800">
+                      {blog.description}
+                    </p>
+                  </CardContent>
+                  <CardFooter>
+                    <CardTags
+                      teamName=""
+                      eventType={blog.categories.join(", ")}
+                    />
+                  </CardFooter>
+                </Card>
+              </Link>
+            </BlurFade>
           ))}
-      </div>
+    </div>
 
-      <div className="mt-8 flex justify-center items-center space-x-4">
-        {currentPage > 1 && (
-          <button
-            onClick={() => handlePageChange(currentPage - 1)}
-            className="text-blue-500 text-lg hover:text-darkblue transition-colors"
-          >
-            Prev
-          </button>
-        )}
-
-        {generatePagination().map((page, index) => (
-          <button
-            key={index}
-            onClick={() => page !== "..." && handlePageChange(Number(page))}
-            className={`text-blue-500 text-lg ${page === currentPage ? "text-darkblue font-semibold" : "hover:text-darkblue"}`}
-            disabled={page === "..." || page === currentPage}
-          >
-            {page}
-          </button>
-        ))}
-
-        {currentPage < totalPages && (
-          <button
-            onClick={() => handlePageChange(currentPage + 1)}
-            className="text-blue-500 text-lg hover:text-darkblue transition-colors"
-          >
-            Next
-          </button>
-        )}
-      </div>
+      {!loading && (
+        <div className="mt-8 flex justify-center items-center space-x-4">
+          {currentPage > 1 && (
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              className="text-blue-500 text-lg hover:text-darkblue transition-colors"
+            >
+              Prev
+            </button>
+          )}
+          {generatePagination().map((page, index) => (
+            <button
+              key={index}
+              onClick={() => page !== "..." && handlePageChange(Number(page))}
+              className={`text-blue-500 text-lg ${
+                page === currentPage
+                  ? "text-darkblue font-semibold"
+                  : "hover:text-darkblue"
+              }`}
+              disabled={page === "..." || page === currentPage}
+            >
+              {page}
+            </button>
+          ))}
+          {currentPage < totalPages && (
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              className="text-blue-500 text-lg hover:text-darkblue transition-colors"
+            >
+              Next
+            </button>
+          )}
+        </div>
+      )}
     </>
   );
 }
 
 export default function BlogList() {
   return (
-<main className="w-full max-w-4xl mx-auto px-8 py-10 flex flex-col gap-8">
-<TitleHeader text="Blog" color="blue" />
-      <Suspense fallback={<div>Loading...</div>}>
-        <BlogListContent />
-      </Suspense>
+    <main className="w-full max-w-4xl mx-auto px-8 py-10 flex flex-col gap-8">
+      <TitleHeader text="Blog" color="blue" />
+      <BlogListContent />
     </main>
   );
 }
